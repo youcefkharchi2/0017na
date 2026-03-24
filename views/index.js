e03241b61815f6d0ddb4a794f3736ffea0e5ab83
youcefkharchi2 <kharchiyoucef10@gmail.com>
2026-03-24 15:57:51 +0100

    Add HTML structure for Discord Bot Dashboard

---
+++ views/index.js
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Dashboard</title>
+  <link rel="stylesheet" href="/style.css">
+</head>
+<body>
+
+<h1>🔥 Discord Bot Dashboard</h1>
+
+<form method="POST" action="/save">
+  <label>Prefix:</label>
+  <input name="prefix" value="<%= settings.prefix %>"><br><br>
+
+  <label>Welcome:</label>
+  <select name="welcome">
+    <option value="on">ON</option>
+    <option value="off">OFF</option>
+  </select><br><br>
+
+  <button type="submit">Save</button>
+</form>
+
+</body>
+</html>
